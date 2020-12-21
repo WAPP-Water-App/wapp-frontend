9b97ec79ea22da95ff8c1530ea0cd67b5469aa62
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-12-21 15:09:59 -0500

    [Settings] Update and add logic to Settings

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -6,12 +6,21 @@ export default function Settings() {
   // settings component
   // basically a form
   //TODO: send data to api endpoint to create/update user data
-  
-  const [age, setAge] = useState();
-  const [weight, setWeight] = useState();
-  const [height, setHeight] = useState();
-  const [gender, setGender] = useState();
-  const [numReminders, setNumReminders] = useState();
+
+  const [age, setAge] = useState(25);
+  const [weight, setWeight] = useState(50);
+  const [height, setHeight] = useState(165);
+  const [startTime, setStartTime] = useState(9);
+  const [endTime, setEndTime] = useState(22);
+  const [numReminders, setNumReminders] = useState(endTime-startTime);
+
+  const renderDropdown = (num) => {
+    const times = new Array(24).fill('');
+    times[num] = true;
+    return times.map((time, index) => (
+      <option value={index} selected={time}>{index}</option>
+    ))
+  }
 
   return (
     <div>
@@ -31,61 +40,58 @@ export default function Settings() {
           <span>{age}</span>
         </div>
         <div className="form-group">
-        <label for="weight">Weight</label>
-        <button onClick={(e)=>setWeight(parseInt(weight)-1)}>+</button>
+          <label for="weight">Weight</label>
           <input
             type="number"
             id="weight"
-W           name="weight"
-W           min="0"
+            W name="weight"
+            W min="0"
             max="500"
+            value={weight}
             onChange={(e) => setWeight(parseInt(e.target.value))}
           />
-          <button onClick={(e)=>setWeight(parseInt(weight)+1)}>+</button>
           <span>{weight} kg</span>
         </div>
         <div className="form-group">
-        <label for="height">Height</label>
-        <button onClick={(e)=>setHeight(parseInt(height)-1)}>+</button>
+          <label for="height">Height</label>
           <input
             type="number"
             id="height"
-W           name="height"
-W           min="0"
+            W name="height"
+            W min="0"
             max="500"
+            value={height}
             onChange={(e) => setHeight(parseInt(e.target.value))}
           />
-          <button onClick={(e)=>setWeight(parseInt(height)+1)}>+</button>
           <span>{height} cm</span>
         </div>
         <div className="form-group">
-        <label for="gender">Gender</label>
-          <button onClick={(e)=>setGender('M')}>Male</button>
-          <button onClick={(e)=>setGender('F')}>Female</button>
-          <button onClick={(e)=>setGender('O')}>Other</button>
-          <span>Selected Gender: {gender} </span>
-        </div>
-        <div className="form-group">
           <label for="startTime">What time do you start your day:</label>
-          <input
+          {/* <input
             type="time"
             id="startTime"
             name="startTime"
             min="09:00"
             max="18:00"
             required
-          />
+          /> */}
+          <select name="startTime" id="startTime" onChange={(e) => setStartTime(parseInt(e.target.value))}>
+            {renderDropdown(startTime)}
+          </select>
         </div>
         <div className="form-group">
           <label for="endTime">What time do you end your day:</label>
-          <input
+          {/* <input
             type="time"
             id="endTime"
             name="endTime"
             min="09:00"
             max="18:00"
             required
-          />
+          /> */}
+          <select name="endTime" id="endTime" onChange={(e) => {if(e.target.value > startTime){setEndTime(parseInt(e.target.value))} else {setEndTime("error: select later end time")}}}>
+            {renderDropdown(endTime)}
+          </select>
         </div>
         <div className="form-group">
           <label for="numReminders">How many reminders do you want a day?</label>
@@ -94,7 +100,7 @@ W           min="0"
             id="numReminders"
             name="numReminders"
             min="1"
-            max="10"
+            max={`${endTime - startTime}`}
             onChange={(e) => setNumReminders(parseInt(e.target.value))}
           />
           <span>#{numReminders}</span>

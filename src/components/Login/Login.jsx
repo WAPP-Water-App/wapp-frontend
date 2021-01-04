360407205d4655b666abb36600c1be8bf8e07e8f
Jenny <finej01@gmail.com>
2021-01-04 15:22:32 -0500

    fixed authorize link

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,12 +5,14 @@ import './login.css'
 export default function Login() {
   // main login page
   // directs them to the authorize api endpoint
+
+
   return (
     <div className = "login-container">
         <Boop rotation={10} timing={150}>
       <div className = "login-bg">
       <img src="/img/clear-water-drop.png" />
-      <a href={`${process.env.REACT_APP_WAPP_WAPI}/authorize`}>login</a>
+      <a href={`${process.env.REACT_APP_WAPP_API}/authorize`}>login</a>
       </div>
       </Boop>
       <p>WATER APP.<br/>

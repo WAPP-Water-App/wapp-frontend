ad0d2f0cae572fab7ba173550a7e7543db266171
Jenny <finej01@gmail.com>
2021-01-06 13:48:00 -0500

    minor fix to footer

--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -5,30 +5,18 @@ export default function Credits() {
   return (
     <div className="credits-container">
       <div>
-        <ul>
-          <li>Made with:React App </li>
-          <li>Assets: Flaticon.com</li>
-        </ul>
+        <a href="https://github.com/WAPP-Water-App">WAPP GITHUB REPO</a>
       </div>
+
       <div>
         <a href="https://github.com/WAPP-Water-App">
           <img src="img/wapp.png" />
         </a>
       </div>
       <div>
-      <a href="https://github.com/WAPP-Water-App">WAPP GITHUB REPO</a>
-      </div>
-
-      <div>
         <ul>
-          <li>Jenny Feng</li>
-          <li>Frank Collins</li>
-        </ul>
-      </div>
-      <div>
-        <ul>
-          <li>Tu Anh Huynh</li>
-          <li>Tu Hoa Huynh</li>
+          <li>Made with: React App </li>
+          <li>Assets: Flaticon.com</li>
         </ul>
       </div>
     </div>
